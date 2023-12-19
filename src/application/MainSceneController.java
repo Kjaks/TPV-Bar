f02cea565da77424c8753371aba3f59e3c530f02
Karolis_portatil <karolisjakas04@gmail.com>
2023-12-19 10:19:11 +0100

    Minor changes

--- src/application/MainSceneController.java
+++ src/application/MainSceneController.java
@@ -2,6 +2,13 @@ package application;
 
 import javafx.fxml.FXML;
 
+import javafx.event.ActionEvent;
+
 public class MainSceneController {
 
+	// Event Listener on Button.onAction
+	@FXML
+	public void aguaBoton(ActionEvent event) {
+		System.out.println("Le has dado al boton del agua!");
+	}
 }
